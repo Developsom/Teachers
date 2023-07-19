6bbfd0e5bfbf6622830702e55b3fbdc354cc1b57
Developsom <100343391+Developsom@users.noreply.github.com>
2023-07-19 16:14:00 +0200

    Finished entity

--- src/main/java/com/example/teachers/model/Teacher.java
+++ src/main/java/com/example/teachers/model/Teacher.java
@@ -2,9 +2,52 @@ package com.example.teachers.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "teachers")
 public class Teacher {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    String firstName;
+    String lastName;
+    LocalDate dob; //LocalDate is used to save dates
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
 }
