11afa794615e1b650e08a9ee422e0cbdec0e6e49
Developsom <100343391+Developsom@users.noreply.github.com>
2023-07-22 16:22:15 +0200

    Fixed dto

--- src/main/java/com/example/teachers/dto/TeacherDto.java
+++ src/main/java/com/example/teachers/dto/TeacherDto.java
@@ -1,5 +1,6 @@
 package com.example.teachers.dto;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
@@ -7,16 +8,16 @@ import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 
 public class TeacherDto {
+    public Long id;
+
 
     @NotBlank  ////Validatie door spring-boot-starter-validation package
     public String firstName;
-
     @Size(min = 3, max = 255)
     public String lastName;
-
     @Past
     public LocalDate dob;
-
+    @Max(value= 100000)
     public int salary;
-    public Long id;
+
 }
